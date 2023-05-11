597577b687ae0068b37e76717efa4be78e816cad
Michael Angelo P. Biag <michaelangelobiag23@GMAIL.COM>
2023-05-12 00:08:55 +0800

    If addition

--- calculator.py
+++ calculator.py
@@ -3,14 +3,18 @@
 
 
 #choosing mathematical operation that will be used based on the user ( Addition(+), Subtraction(-), Multiplication(x), Division(/))
-math_operator = input("Enter an Operator ( Addition(+), Subtraction(-), Multiplication(x), Division(/))")
+math_operator = input("Enter an Operator ( Addition(+), Subtraction(-), Multiplication(x), Division(/)) : ")
 
 #enter First number and Second number
 firstnumber = float(input("Enter the 1st number : ")) 
 secondnumber = float(input("Enter the 2nd number : ")) 
 
 #If addition
- 
+if math_operator == "+":
+    sum = firstnumber + secondnumber
+    print(round(sum, 4))
+
+
 #If subtraction
 
 #If multiplication
