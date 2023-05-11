517011d1fa151481c48195e338790c6dfe226574
Michael Angelo P. Biag <michaelangelobiag23@GMAIL.COM>
2023-05-12 01:23:31 +0800

    adding exceptions

--- calculator.py
+++ calculator.py
@@ -1,14 +1,24 @@
 #Michael Angelo P. Biag
 #BSCOE 1-4
 
-def main():
+def calculator():
+    print("\n")
 
     #choosing mathematical operation that will be used based on the user ( Addition(+), Subtraction(-), Multiplication(*), Division(/))
     math_operator = input("\033[1;32m""Enter an Operator (+ - * /) : ")
 
-    #enter First number and Second number
-    firstnumber = float(input("\033[1;31m""Enter the 1st number : ")) 
-    secondnumber = float(input("Enter the 2nd number : ")) 
+    #exceptions
+    try: 
+        #enter First number and Second number
+        firstnumber = float(input("\033[1;35m""Enter the 1st number : ")) 
+
+        secondnumber = float(input("\033[1;35m""Enter the 2nd number : ")) 
+    except ValueError as ERROR:  
+         print("\033[1;31m""Invalid number input\n")
+         print(ERROR)
+         print("\nTry again")
+         return
+
 
     #If addition
     if math_operator == "+":
@@ -33,20 +43,33 @@ def main():
 
     #If division
     elif math_operator == "/":
-        the_quotient = firstnumber / secondnumber
-        print("\033[1;36m" +"=" * 6)
-        print(round(the_quotient, 4))
-        print("=" * 6)
+    #exception for division, if the user entered zero(0) as a divisor(2nd number)    
+        try:
+
+            the_quotient = firstnumber / secondnumber
+            print("\033[1;36m" +"=" * 6)
+            print(round(the_quotient, 4))
+            print("=" * 6)
+
+        except ZeroDivisionError as ERROR:
+            print("\033[1;31m""Invalid equation\n")
+            print(ERROR)
+            print("\nTry again")
+            return
 
     else:
         print(f"{math_operator} is not a valid operator")
 
+
+
 # Ask the user if they want to continue on using a calculator (YES/NO)
+
     Repeat = input("\033[1;33m""do you want to continue on using the calculator (YES/NO) : ").upper()
     if Repeat == "YES":
-            main()
+            calculator()
     else:
             print("THANK YOU FOR USING THE CALCULATOR!!")
             exit()
-main()
+while True:
+    calculator()
 
