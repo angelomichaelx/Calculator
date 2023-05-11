22d4312900f1d3e4328f4bd54b2c48cc5f4983f2
Michael Angelo P. Biag <michaelangelobiag23@GMAIL.COM>
2023-05-11 23:49:07 +0800

    pseudocode

---
+++ calculator.py
@@ -0,0 +1,11 @@
+#Michael Angelo P. Biag
+#BSCOE 1-4
+
+
+#choosing mathematical operation that will be used based on the user ( Addition(+), Subtraction(-), Multiplication(x), Division(/))
+#enter First number and Second number
+#If addition
+#If subtraction
+#If multiplication
+#If division
+# Ask the user if they want to continue on using mini calculator (YES/NO)
