9e126588c5f9589d58e8fee0a9b6c9ff74eae566
Michael Angelo P. Biag <michaelangelobiag23@GMAIL.COM>
2023-05-11 23:58:26 +0800

    choosing mathematical operation that will be used based on the user

--- calculator.py
+++ calculator.py
@@ -3,9 +3,15 @@
 
 
 #choosing mathematical operation that will be used based on the user ( Addition(+), Subtraction(-), Multiplication(x), Division(/))
+math_operator = input("Enter an Operator ( Addition(+), Subtraction(-), Multiplication(x), Division(/))")
 #enter First number and Second number
+
 #If addition
+ 
 #If subtraction
+
 #If multiplication
+
 #If division
+
 # Ask the user if they want to continue on using mini calculator (YES/NO)
